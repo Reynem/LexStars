e19e6726d9d8a30b4b15a88f071806948d36936b
Kerim_Lex <lexmeins1@gmail.com>
2025-04-05 22:19:05 +0500

    fixed model and gun issues

--- src/main/java/com/reynem/weapons/RicoGun.java
+++ src/main/java/com/reynem/weapons/RicoGun.java
@@ -1,6 +1,5 @@
 package com.reynem.weapons;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.Item;
@@ -19,37 +18,51 @@ public class RicoGun extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
+        ItemStack stack = user.getStackInHand(hand);
+        boolean hasAmmo = user.getAbilities().creativeMode || user.getInventory().count(Items.ARROW) > 0;
 
-        //потом будут кастомные патроны
-        boolean hasAmmo = !user.getAbilities().creativeMode && user.getInventory().count(Items.ARROW) > 0;
+        if (!hasAmmo) return TypedActionResult.fail(stack);
 
-        if (hasAmmo || user.getAbilities().creativeMode){
-            if (!world.isClient){
-                if (!user.getAbilities().creativeMode){
-                    ItemStack ammoStack = user.getInventory().getStack(user.getInventory().getSlotWithStack(new ItemStack(Items.ARROW)));
-                    if (!ammoStack.isEmpty()){
-                        ammoStack.decrement(1);
-                        if(ammoStack.isEmpty()){
-                            user.getInventory().removeStack(user.getInventory().getSlotWithStack(new ItemStack(Items.ARROW)));
-                        }
-                    } else{
-                        return TypedActionResult.fail(itemStack);
+        if (!world.isClient) {
+            if (!user.getAbilities().creativeMode) {
+                ItemStack ammo = user.getInventory().getStack(
+                        user.getInventory().getSlotWithStack(new ItemStack(Items.ARROW))
+                );
+                if (!ammo.isEmpty()) {
+                    ammo.decrement(1);
+                    if (ammo.isEmpty()) {
+                        user.getInventory().removeStack(
+                                user.getInventory().getSlotWithStack(new ItemStack(Items.ARROW))
+                        );
                     }
+                } else {
+                    return TypedActionResult.fail(stack);
                 }
-
-                ArrowEntity arrowEntity = new ArrowEntity(EntityType.ARROW, world);
-                arrowEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 3.0F, 1.0F);
-                arrowEntity.setDamage(10);
-                world.spawnEntity(arrowEntity);
-                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0f, 1.0f / (world.getRandom().nextFloat() * 0.4f + 1.2f) + 0.5f);
-                user.getItemCooldownManager().set(this, 10);
             }
 
-            return TypedActionResult.success(itemStack);
-        } else{
-            return TypedActionResult.fail(itemStack);
+            ArrowEntity arrow = new ArrowEntity(
+                    world,
+                    user.getX(), user.getEyeY(), user.getZ(),
+                    new ItemStack(Items.ARROW),
+                    null
+            );
+            arrow.setOwner(user);
+            arrow.setDamage(10);
+            arrow.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 4.0F, 0.0F);
+
+            world.spawnEntity(arrow);
+            world.playSound(
+                    null,
+                    user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.ENTITY_ARROW_SHOOT,
+                    SoundCategory.PLAYERS,
+                    1.0f,
+                    1.0f / (world.getRandom().nextFloat() * 0.4f + 1.2f) + 0.5f
+            );
+            user.getItemCooldownManager().set(this, 10);
         }
 
+        return TypedActionResult.success(stack);
     }
+
 }
